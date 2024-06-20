d2cc70053bb2cf2db6e3f1a98dd69077e9f4a747
Ahmet Korkmaz <ahmetkorkmaz3453@gmail.com>
2024-06-20 11:09:27 +0300

    update date filter

--- src/helpers/filter.js
+++ src/helpers/filter.js
@@ -6,8 +6,7 @@ const getFilters = (queryParams) => {
         refundable,
         min_amount,
         max_amount,
-        start_date,
-        end_date,
+        date,
     } = queryParams
 
     const filters = {}
@@ -38,18 +37,48 @@ const getFilters = (queryParams) => {
         }
     }
 
-    if (start_date) {
-        filters.created_at = { $gte: new Date(start_date) }
-    }
-
-    if (end_date) {
-        filters.created_at = {
-            ...filters.created_at,
-            $lte: new Date(end_date),
-        }
+    if (date) {
+        filters.created_at = getQueryForDateFilter(date)
     }
 
     return filters
+}
+
+function getQueryForDateFilter(value) {
+    const now = new Date()
+
+    switch (value) {
+        case 'today':
+            const startOfToday = new Date(now)
+            startOfToday.setHours(0, 0, 0, 0)
+            return { createdAt: { $gte: startOfToday } }
+        case 'yesterday':
+            const startOfYesterday = new Date(now)
+            startOfYesterday.setDate(startOfYesterday.getDate() - 1)
+            startOfYesterday.setHours(0, 0, 0, 0)
+
+            const endOfYesterday = new Date(now)
+            endOfYesterday.setDate(endOfYesterday.getDate() - 1)
+            endOfYesterday.setHours(23, 59, 59, 999)
+
+            return {
+                createdAt: { $gte: startOfYesterday, $lte: endOfYesterday },
+            }
+        case 'last_7_days':
+            const sevenDaysAgo = new Date(now)
+            sevenDaysAgo.setDate(sevenDaysAgo.getDate() - 7)
+            return { createdAt: { $gte: sevenDaysAgo } }
+        case 'last_30_days':
+            const thirtyDaysAgo = new Date(now)
+            thirtyDaysAgo.setDate(thirtyDaysAgo.getDate() - 30)
+            return { createdAt: { $gte: thirtyDaysAgo } }
+        case 'last_1_years':
+            const oneYearAgo = new Date(now)
+            oneYearAgo.setFullYear(oneYearAgo.getFullYear() - 1)
+            return { createdAt: { $gte: oneYearAgo } }
+        default:
+            return {}
+    }
 }
 
 module.exports = { getFilters }
